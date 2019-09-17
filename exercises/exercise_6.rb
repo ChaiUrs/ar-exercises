3838ec18b0f4ef6cf27bd913fa92e1e902ba8507
ChaiUrs <mysorechaitra@gmail.com>
2019-09-17 05:13:11 +0000

    Added code to lib and completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Nima", last_name: "Boscarino", hourly_rate: 80)
+@store1.employees.create(first_name: "Travis", last_name: "Borsa", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 120)
+@store2.employees.create(first_name: "Kelly", last_name: "Fox", hourly_rate: 140)
+@store2.employees.create(first_name: "Karl", last_name: "Jensen", hourly_rate: 160)
